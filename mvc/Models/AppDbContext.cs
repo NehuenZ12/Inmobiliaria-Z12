bd2c5729dfa9ca8f72202a264bfed980afd701a0
owengmz <heber12398@gmail.com>
2026-09-03 12:04:25 -0300

    Mapea Inquilino en AppDbContext

--- mvc/Models/AppDbContext.cs
+++ mvc/Models/AppDbContext.cs
@@ -2,98 +2,127 @@ using Microsoft.EntityFrameworkCore;
 
 namespace mvc.Models
 {
-    public class AppDbContext : DbContext
+  public class AppDbContext : DbContext
+  {
+    public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options)
     {
-        public AppDbContext(DbContextOptions<AppDbContext> options)
-            : base(options)
-        {
-        }
-
-        public DbSet<Propietario> Propietarios { get; set; }
-
-        public DbSet<Inmueble> Inmuebles { get; set; }
-
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            // Tabla Propietario
-            modelBuilder.Entity<Propietario>()
-                .ToTable("propietario");
-
-            modelBuilder.Entity<Propietario>()
-                .Property(p => p.Id)
-                .HasColumnName("id");
-
-            modelBuilder.Entity<Propietario>()
-                .Property(p => p.Nombre)
-                .HasColumnName("nombre");
-
-            modelBuilder.Entity<Propietario>()
-                .Property(p => p.Apellido)
-                .HasColumnName("apellido");
-
-            modelBuilder.Entity<Propietario>()
-                .Property(p => p.Dni)
-                .HasColumnName("dni");
-
-            modelBuilder.Entity<Propietario>()
-                .Property(p => p.Telefono)
-                .HasColumnName("telefono");
-
-            modelBuilder.Entity<Propietario>()
-                .Property(p => p.Email)
-                .HasColumnName("email");
-
-
-            // INMUEBLE
-
-            modelBuilder.Entity<Inmueble>().ToTable("inmueble");
-
-            modelBuilder.Entity<Inmueble>()
-                .Property(i => i.Id)
-                .HasColumnName("id");
-
-            modelBuilder.Entity<Inmueble>()
-                .Property(i => i.Direccion)
-                .HasColumnName("direccion");
-
-            modelBuilder.Entity<Inmueble>()
-                .Property(i => i.Cupo)
-                .HasColumnName("cupo");
-
-            modelBuilder.Entity<Inmueble>()
-                .Property(i => i.Tipo)
-                .HasColumnName("tipo");
-
-            modelBuilder.Entity<Inmueble>()
-                .Property(i => i.Latitud)
-                .HasColumnName("latitud");
-
-            modelBuilder.Entity<Inmueble>()
-                .Property(i => i.Longitud)
-                .HasColumnName("longitud");
-
-            modelBuilder.Entity<Inmueble>()
-                .Property(i => i.PrecioPorDia)
-                .HasColumnName("precio_por_dia");
-
-            modelBuilder.Entity<Inmueble>()
-                .Property(i => i.PorcentajeReserva)
-                .HasColumnName("porcentaje_reserva");
-
-            modelBuilder.Entity<Inmueble>()
-                .Property(i => i.Disponible)
-                .HasColumnName("disponible");
-
-            modelBuilder.Entity<Inmueble>()
-                .Property(i => i.PropietarioId)
-                .HasColumnName("propietario_id");
-
-
-            // Relacion: un propietario puede tener varios inmuebles
-            modelBuilder.Entity<Inmueble>()
-                .HasOne(i => i.Propietario)
-                .WithMany()
-                .HasForeignKey(i => i.PropietarioId);
-        }
     }
+
+    public DbSet<Propietario> Propietarios { get; set; }
+
+    public DbSet<Inmueble> Inmuebles { get; set; }
+    public DbSet<Inquilino> Inquilinos { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+      // Tabla Propietario
+      modelBuilder.Entity<Propietario>()
+          .ToTable("propietario");
+
+      modelBuilder.Entity<Propietario>()
+          .Property(p => p.Id)
+          .HasColumnName("id");
+
+      modelBuilder.Entity<Propietario>()
+          .Property(p => p.Nombre)
+          .HasColumnName("nombre");
+
+      modelBuilder.Entity<Propietario>()
+          .Property(p => p.Apellido)
+          .HasColumnName("apellido");
+
+      modelBuilder.Entity<Propietario>()
+          .Property(p => p.Dni)
+          .HasColumnName("dni");
+
+      modelBuilder.Entity<Propietario>()
+          .Property(p => p.Telefono)
+          .HasColumnName("telefono");
+
+      modelBuilder.Entity<Propietario>()
+          .Property(p => p.Email)
+          .HasColumnName("email");
+
+
+      // INMUEBLE
+
+      modelBuilder.Entity<Inmueble>().ToTable("inmueble");
+
+      modelBuilder.Entity<Inmueble>()
+          .Property(i => i.Id)
+          .HasColumnName("id");
+
+      modelBuilder.Entity<Inmueble>()
+          .Property(i => i.Direccion)
+          .HasColumnName("direccion");
+
+      modelBuilder.Entity<Inmueble>()
+          .Property(i => i.Cupo)
+          .HasColumnName("cupo");
+
+      modelBuilder.Entity<Inmueble>()
+          .Property(i => i.Tipo)
+          .HasColumnName("tipo");
+
+      modelBuilder.Entity<Inmueble>()
+          .Property(i => i.Latitud)
+          .HasColumnName("latitud");
+
+      modelBuilder.Entity<Inmueble>()
+          .Property(i => i.Longitud)
+          .HasColumnName("longitud");
+
+      modelBuilder.Entity<Inmueble>()
+          .Property(i => i.PrecioPorDia)
+          .HasColumnName("precio_por_dia");
+
+      modelBuilder.Entity<Inmueble>()
+          .Property(i => i.PorcentajeReserva)
+          .HasColumnName("porcentaje_reserva");
+
+      modelBuilder.Entity<Inmueble>()
+          .Property(i => i.Disponible)
+          .HasColumnName("disponible");
+
+      modelBuilder.Entity<Inmueble>()
+          .Property(i => i.PropietarioId)
+          .HasColumnName("propietario_id");
+
+      // INQUILINO
+
+      modelBuilder.Entity<Inquilino>().ToTable("inquilino");
+
+      modelBuilder.Entity<Inquilino>()
+          .Property(i => i.Id)
+          .HasColumnName("id");
+
+      modelBuilder.Entity<Inquilino>()
+          .Property(i => i.Nombre)
+          .HasColumnName("nombre");
+
+      modelBuilder.Entity<Inquilino>()
+          .Property(i => i.Apellido)
+          .HasColumnName("apellido");
+
+      modelBuilder.Entity<Inquilino>()
+          .Property(i => i.Dni)
+          .HasColumnName("dni");
+
+      modelBuilder.Entity<Inquilino>()
+          .Property(i => i.Telefono)
+          .HasColumnName("telefono");
+
+      modelBuilder.Entity<Inquilino>()
+          .Property(i => i.Email)
+          .HasColumnName("email");
+
+
+      // Relacion: un propietario puede tener varios inmuebles
+      modelBuilder.Entity<Inmueble>()
+          .HasOne(i => i.Propietario)
+          .WithMany()
+          .HasForeignKey(i => i.PropietarioId);
+    }
+  }
 }
